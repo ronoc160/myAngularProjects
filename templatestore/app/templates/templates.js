790bc4472d743ca8b41e307d2893ec5e7e6fb15b
Conor Byrne <conor@tuimedia.com>
2016-11-22 13:42:54 +0000

    Angular filter with unique ID and json with angular services

---
+++ templatestore/app/templates/templates.js
@@ -0,0 +1,36 @@
+'use strict';
+
+angular.module('templateStore.templates', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider
+  .when('/templates', {
+    templateUrl: 'templates/templates.html',
+    controller: 'TemplateCtrl'
+  }).
+  when('/templates/:templateId', {
+    templateUrl: 'templates/templates-details.html',
+    controller: 'TemplateDetailsCtrl'
+  })
+
+}])
+
+.controller('TemplateCtrl', ['$scope', '$http', function($scope, $http) {
+    $http.get('json/templates.json').success(function(data){
+      $scope.templates = data;
+    });
+}])
+
+.controller('TemplateDetailsCtrl', ['$scope', '$http', '$routeParams', '$filter', function($scope, $http, $routeParams, $filter) {
+  var templateId = $routeParams.templateId;
+  $http.get('json/templates.json').success(function(data){
+    $scope.template = $filter('filter')(data, function(d){
+      return d.id == templateId;
+    })[0];
+    $scope.mainImage = $scope.template.images[0].name;
+  });
+
+  $scope.setImage = function (image){
+    $scope.mainImage = image.name;
+  }
+}])
